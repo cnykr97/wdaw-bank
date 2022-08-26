1c1def63dbd936bb1f69492e0939b04a9e6a73be
Can Yakar <canyakar9797@gmail.com>
2022-08-26 16:39:38 +0300

    business section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles, content }) => {
     return (
-        <div>Button</div>
+        <button type='button' className={`${styles} py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none`}>{content}</button>
     )
 }
 
