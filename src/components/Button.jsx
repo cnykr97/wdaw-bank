a14636b61292a3ff76961ee87d721f4f23ef9252
Can Yakar <canyakar9797@gmail.com>
2022-09-14 19:26:43 +0300

    creating hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Button = ({ styles, content }) => {
+    const [isHover, setIsHover] = useState(false)
     return (
-        <button type='button' className={`${styles} py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none`}>{content}</button>
+        <button type='button' className={`${styles} py-4 px-6 font-poppins font-medium text-[18px] ${isHover ? "text-white" : "text-primary"} bg-blue-gradient rounded-[10px] outline-none`}
+            onMouseOver={() => { setIsHover((prev) => (!prev)) }}
+            onMouseOut={() => { setIsHover((prev) => (!prev)) }}
+        >{content}</button>
     )
 }
 
